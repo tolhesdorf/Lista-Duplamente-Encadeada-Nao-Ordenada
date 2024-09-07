725b997895e213ae9f19d0ebc1ec89bbe5060170
Tolhesdorf <ph.tolksdorf@gmail.com>
2024-09-07 12:34:40 -0300

    Correção de identação;

--- LD_Não_Ordenada/src/ld_não_ordenada/projeto/Main.java
+++ LD_Não_Ordenada/src/ld_não_ordenada/projeto/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         while(LD.Op != 7){
-         LD.Menu();
-        LD.Funcionalidades();
+            LD.Menu();
+            LD.Funcionalidades();
         }
         System.out.println("\nEncerrando...");
     }
